a5fa068b97014f879e883cb4baaa93563132cf7d
tealytee <95439169+tealytee@users.noreply.github.com>
2024-07-09 16:23:31 +0700

    Create dap an bai tap.cpp

---
+++ buoi 4/dap an bai tap.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout << "Yeu to quoc, yeu dong bao"<< endl;
+cout << "Hoc tap tot, lao dong tot"<< endl;
+cout << "Doan ket tot, ky luat tot"<< endl;
+cout << "Giu gin ve sinh that tot"<< endl;
+cout << "Khiem ton, that tha, dung cam";
+
+ 
+return 0;
+}
+
