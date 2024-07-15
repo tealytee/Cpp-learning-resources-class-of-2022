a808ea6767d2f1359b0cf5e9c805df57f6fbb931
tealytee <95439169+tealytee@users.noreply.github.com>
2024-07-15 11:31:02 +0700

    Create dap an.cpp

---
+++ buoi 6/dap an.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b,c;
+    cout << "Enter a";
+    cin >> a;
+    cout << "Enter b";
+    cin >> b;
+    cout << "Enter c";
+    cin >> c;
+    cout << a << " + " << b <<" + " << c <<" " = " << a+b+c;
+}
