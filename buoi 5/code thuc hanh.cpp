d1bad8fbfb3ef2f8f6c084e9cae7f372cf1d77da
tealytee <95439169+tealytee@users.noreply.github.com>
2024-07-15 11:24:32 +0700

    Create code thuc hanh.cpp

---
+++ buoi 5/code thuc hanh.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout <<" thong tin dau tien " << "thong tin thu hai"<< endl;
+    //đây là các viết thứ nhất
+    cout << "thong tin thu 3";
+    cout << "thong tin thu 4";    
+    //Đây là cách viết thứ hai
+}
